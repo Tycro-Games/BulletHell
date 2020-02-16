1d57ac151614770c62c0d4ab8df6cec98c6061a3
OneBogdan01 <31468440+OneBogdan01@users.noreply.github.com>
2020-02-17 01:06:09 +0200

    I dont fucking know

---
+++ UnityProject/Assets/Scripts/PathGeneration/PathGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PathGenerator
+{
+
+    public static Path CreatePath(Vector3 startPos, Vector3 endPos)
+    {
+        Path path = new Path(Vector3.zero);
+        path.MovePoint(1, startPos);
+        path.MovePoint(1, startPos);
+        return path;
+    }
+
+}
