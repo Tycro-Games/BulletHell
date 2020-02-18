496c97a316b797dab76a825acc78b9344edd5778
OneBogdan01 <31468440+OneBogdan01@users.noreply.github.com>
2020-02-18 22:09:00 +0200

    da

---
+++ Bullet Hell/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speedMovement=0.0f;
+    Rigidbody rb;
+    PlayerInput input;
+    Vector2 movement;
+    private void Awake()
+    {
+        input = new PlayerInput();
+        input.PlayerMovement.Movement.performed += ctx => movement = ctx.ReadValue<Vector2>();
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = movement.x;
+        float v = movement.y;
+         Vector3 velocity = new Vector3(h, 0, v);
+        rb.MovePosition(rb.position + velocity*Time.fixedDeltaTime*speedMovement);
+    }
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+}
