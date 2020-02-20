3d8057f969ef26dd2bee9d549b9d3e883153ce85
OneBogdan01 <bogdan.game.development@gmail.com>
2020-02-20 23:10:48 +0200

    THis shit goes to master

---
+++ Bullet Hell/Assets/Scripts/Spawners/EnemySpawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    
+    public EnemyInfo enemyInfo = null; 
+    private GameObject spawnedObject;
+
+    public void Spawn ( )
+    {
+        spawnedObject = Instantiate (enemyInfo.EnemyPrefab, transform.position, Quaternion.identity);
+        spawnedObject.transform.SetParent (transform);
+    }
+}
