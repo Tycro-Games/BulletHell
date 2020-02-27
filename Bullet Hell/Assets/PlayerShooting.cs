a652b4a26897208feb2b6066f69bea74d304fed0
OneBogdan01 <bogdan.game.development@gmail.com>
2020-02-28 00:38:44 +0200

    make the fucking input work

---
+++ Bullet Hell/Assets/PlayerShooting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    private bool shooting = false;
+    
+    public void Shoot ()
+    {
+
+        
+        
+
+        if (shooting)
+        { 
+            
+            StartCoroutine (Shooting ());
+        }
+    }
+    IEnumerator Shooting ()
+    {
+        Debug.Log ("Shooting");
+        while (shooting)
+        {
+            Debug.Log ("Shoot");
+            yield return null;
+        }
+
+    }
+    
+
+
+
+
+}
---
+++ Bullet Hell/Assets/Scripts/Player/InputForPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent (typeof (PlayerInput))]
+public class InputForPlayer : MonoBehaviour
+{
+    protected PlayerInput inputMaster = null;
+    public virtual void Awake ()
+    {
+        inputMaster = GetComponent<PlayerInput> ();
+    }
+}
