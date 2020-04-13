32a750b51cf4a69a7563d4e33e7e5e15abafb3f3
OneBogdan01 <bogdan.game.development@gmail.com>
2020-04-13 20:16:06 +0300

    MASTERSOFAR

---
+++ Bullet Hell/Assets/Scripts/Enemies/Functionality/EnemySpawner.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public void Spawn (GameObject enemyPrefab)
+    {
+        GameObject enemy = Spawner.Spawn (enemyPrefab, transform, transform);       
+    }
+}
--- Bullet Hell/Assets/Scripts/Enemies/Funtionality/EnemySpawner.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class EnemySpawner : MonoBehaviour
-{
-    public void Spawn (GameObject enemyPrefab)
-    {
-        GameObject enemy = Spawner.Spawn (enemyPrefab, transform, transform);       
-    }
-}
