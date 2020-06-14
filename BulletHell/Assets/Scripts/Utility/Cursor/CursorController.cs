bab89b070a988e9422e30e38c3d316fcd65d7538
NiceGuy <bogdan.game.development@gmail.com>
2020-06-14 21:33:13 +0300

    bug la camera

--- BulletHell/Assets/Scripts/Utility/Cursor/CursorController.cs
+++ BulletHell/Assets/Scripts/Utility/Cursor/CursorController.cs
@@ -7,21 +7,24 @@ public class CursorController : MonoBehaviour
     private PlayerInput input;
     [SerializeField]
     private float radius = 5.0f;
+
+
     private void Start ()
     {
         Cursor.visible = false;
+
         input = GetComponentInParent<PlayerInput> ();//ref to the player input
     }
     private void Update ()
     {
         if (input.currentControlScheme == "Keyboard+Mouse")
-            transform.position = MousePosition ();
+            transform.position = MousePosition();
     }
-
+   
     public static Vector2 MousePosition ()
     {
         Vector2 CursorPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-          return CursorPos;
+        return CursorPos;
     }
     private void OnDrawGizmos ()
     {
