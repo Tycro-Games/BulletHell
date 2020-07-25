4d0e325ed377fe2606cd55315e33b46c02ba9396
NiceGuy <bogdan.game.development@gmail.com>
2020-07-25 18:25:11 +0300

    Integrare Rob

--- BulletHell/Assets/Scripts/Enemies/Funtionality/EnemySpawner.cs
+++ BulletHell/Assets/Scripts/Enemies/Funtionality/EnemySpawner.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
-{
-    public void Spawn (GameObject enemyPrefab)
+{    public void Spawn (GameObject enemyPrefab)
     {
         GameObject enemy = Spawner.Spawn (enemyPrefab, transform.position,transform.rotation, transform);       
     }
+
 }
