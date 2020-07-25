62983d08f451e5545bf5709d1fa3cc0f27403db8
SoulSnorter <petrerobertcristian@gmail.com>
2020-07-25 17:54:17 +0300

    partea mea de proiect

---
+++ Bullet Hell/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int width = 30;
+    public int height = 30;
+    public int chance = 50;
+    public int numberOfRooms = 80;
+    public float bossChance = 10;
+    public float treasureChance = 7;
+    public GameObject roomObj;
+    public GameObject roomSpawner;
+    FuckingRoom[,] rooms;
+    public Transform mapRoot;
+
+    ChangeCamera cameraChanger;
+
+    public int roomSpriteWidth = 12;
+    public int roomSpriteHeight = 6;
+
+    public int x;
+    public int y;
+
+    int count = 0;
+
+    void Start()
+    {
+        x = width / 2;
+        y = height / 2;
+        CreateRooms();
+        SetDoors();
+        SpawnMap();
+        SpawnRooms();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey("r"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if(Input.GetKeyDown("w") && y + 1 < height && rooms[x, y + 1].empty == false)
+        {
+            rooms[x, y].sexuallyActive = false;
+            y++;      
+            rooms[x, y].sexuallyActive = true;
+            SpawnMap();
+        }
+        if (Input.GetKeyDown("a") && x - 1 > 0 && rooms[x - 1, y].empty == false)
+        {
+            rooms[x, y].sexuallyActive = false;
+            x--;
+            rooms[x, y].sexuallyActive = true;
+            SpawnMap();
+        }
+        if (Input.GetKeyDown("s") && y - 1 > 0 && rooms[x, y - 1].empty == false)
+        {
+            rooms[x, y].sexuallyActive = false;
+            y--;
+            rooms[x, y].sexuallyActive = true;
+            SpawnMap();
+        }
+        if (Input.GetKeyDown("d") && x + 1 < width && rooms[x + 1, y].empty == false)
+        {
+            rooms[x, y].sexuallyActive = false;
+            x++;
+            rooms[x, y].sexuallyActive = true;
+            SpawnMap();
+        }
+    }
+
+    void CreateRooms()
+    {
+        rooms = new FuckingRoom[width, height];
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                rooms[i, j] = new FuckingRoom();
+                if (i == width / 2 && j == height / 2)
+                {
+                    rooms[i, j].type = 1;
+                }            
+            }
+        }
+        GenerateRoom(width / 2, height / 2);
+        if (numberOfRooms > width * height)
+            numberOfRooms = width * height;
+        if (count != numberOfRooms)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
+    }
+
+    void GenerateRoom(int x, int y)
+    {
+        rooms[x, y].empty = false;
+        rooms[x, y].searched = true;
+        if(x != width / 2 && y != height / 2)
+        {
+            if (Random.Range(0, 100) < bossChance)
+            {
+                rooms[x, y].type = 3;
+            }
+            if (Random.Range(0, 100) < treasureChance)
+            {
+                rooms[x, y].type = 4;
+            }
+        }
+        
+        count++;
+        if (count != numberOfRooms && Random.Range(0, 100) < chance && x > 0 && rooms[x - 1, y].searched == false)
+        {
+            GenerateRoom(x - 1, y);
+        }
+        if (count != numberOfRooms && Random.Range(0, 100) < chance && y > 0 && rooms[x, y - 1].searched == false)
+        {
+           GenerateRoom(x, y - 1);
+        }
+        if (count != numberOfRooms && Random.Range(0, 100) < chance && x < width - 1 && rooms[x + 1, y].searched == false)
+        {
+            GenerateRoom(x + 1, y);
+        }
+        if (count != numberOfRooms && Random.Range(0, 100) < chance && y < height - 1 && rooms[x, y + 1].searched == false)
+        {
+            GenerateRoom(x, y + 1);
+        }
+    }
+
+    void SetDoors()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                rooms[i, j].x = i;
+                rooms[i, j].y = j;
+
+                if (rooms[i, j].empty == false)
+                {
+                    if (i > 0 && rooms[i - 1, j].empty == false)
+                    {
+                        rooms[i, j].doorLeft = true;
+                    }
+                    if (i < width - 1 && rooms[i + 1, j].empty == false)
+                    {
+                        rooms[i, j].doorRight = true;
+                    }
+                    if (j > 0 && rooms[i, j - 1].empty == false)
+                    {
+                        rooms[i, j].doorBot = true;
+                    }
+                    if (j < height - 1 && rooms[i, j + 1].empty == false)
+                    {
+                        rooms[i, j].doorTop = true;
+                    }
+                }
+            }
+        }
+    }
+
+    void SpawnMap()
+    {
+        foreach (Transform child in mapRoot.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        foreach (FuckingRoom room in rooms)
+        {
+            if(room.empty == false)
+            {
+                Vector2 spawnPos = new Vector2(room.x * (roomSpriteWidth + roomSpriteWidth / 3), room.y * (roomSpriteHeight + roomSpriteHeight / 3));
+                SpriteMapping mapper = Object.Instantiate(roomObj, spawnPos, Quaternion.identity).GetComponent<SpriteMapping>();
+
+                if (room.sexuallyActive)
+                    mapper.type = 2;
+                else
+                    mapper.type = room.type;
+                mapper.doorTop = room.doorTop;
+                mapper.doorBot = room.doorBot;
+                mapper.doorRight = room.doorRight;
+                mapper.doorLeft = room.doorLeft;
+                mapper.gameObject.transform.parent = mapRoot;
+            }
+            
+        }
+    }
+
+    void SpawnRooms()
+    {
+        foreach (FuckingRoom room in rooms)
+        {
+            if (room.empty == false)
+            {
+                Vector2 roomSpawnPos = new Vector2(room.x * 50f, room.y * 50f);
+                ActualFuckingRoomGenerator roomGen = Object.Instantiate(roomSpawner, roomSpawnPos, Quaternion.identity).GetComponent<ActualFuckingRoomGenerator>();
+                roomGen.doorTop = room.doorTop;
+                roomGen.doorBot = room.doorBot;
+                roomGen.doorRight = room.doorRight;
+                roomGen.doorLeft = room.doorLeft;
+                roomGen.roomType = room.type;
+            }
+
+        }
+    }
+}
---
+++ Bullet Hell/Assets/Scripts/SpriteMapping.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMapping : MonoBehaviour
+{
+    public Sprite roomUpDownLeftRight, // 1
+    roomUpDownLeft,      // 2
+    roomUpDownRight,     // 3
+    roomUpDown,          // 4
+    roomUp,              // 5
+    roomDown,            // 6
+    roomUpLeft,          // 7
+    roomUpRight,         // 8
+    roomUpLeftRight,     // 9
+    roomDownLeft,        // 10
+    roomDownRight,       // 11
+    roomDownLeftRight,   // 12
+    roomRightLeft,       // 13
+    roomRight,           // 14
+    roomLeft;            // 15
+
+    public bool doorTop = true, doorBot = true, doorRight = true, doorLeft = true;
+
+    public int type = 0;
+
+    public Color normalColor,startColor,actualColor,bossColor,treasureColor;
+    Color mainColor;
+
+    SpriteRenderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        SpritePicker();
+        ColorPicker();
+    }
+
+    void SpritePicker()
+    {
+        // urmeaza cel mai frumos(scarbos) cod pe care l-ai vazut vreodata
+        if(doorTop)
+        {
+            if(doorBot)
+            {
+                if(doorRight)
+                {
+                    if(doorLeft)
+                    {
+                        rend.sprite = roomUpDownLeftRight;
+                    }
+                    else
+                    {
+                        rend.sprite = roomUpDownRight;
+                    }
+                }
+                else
+                {
+                    if(doorLeft)
+                    {
+                        rend.sprite = roomUpDownLeft;
+                    }
+                    else
+                    {
+                        rend.sprite = roomUpDown;
+                    }
+                }
+            }
+            else
+            {
+                if(doorRight)
+                {
+                    if(doorLeft)
+                    {
+                        rend.sprite = roomUpLeftRight;
+                    }
+                    else
+                    {
+                        rend.sprite = roomUpRight;
+                    }
+                }
+                else
+                {
+                    if(doorLeft)
+                    {
+                        rend.sprite = roomUpLeft;
+                    }
+                    else
+                    {
+                        rend.sprite = roomUp;
+                    }
+                }
+            }
+        }
+        else
+        {
+            if(doorBot)
+            {
+                if (doorRight)
+                {
+                    if (doorLeft)
+                    {
+                        rend.sprite = roomDownLeftRight;
+                    }
+                    else
+                    {
+                        rend.sprite = roomDownRight;
+                    }
+                }
+                else
+                {
+                    if (doorLeft)
+                    {
+                        rend.sprite = roomDownLeft;
+                    }
+                    else
+                    {
+                        rend.sprite = roomDown;
+                    }
+                }
+            }
+            else
+            {
+                if (doorRight)
+                {
+                    if(doorLeft)
+                    {
+                        rend.sprite = roomRightLeft;
+                    }
+                    else
+                    {
+                        rend.sprite = roomRight;
+                    }
+                }
+                else
+                {
+                    rend.sprite = roomLeft;
+                }
+            }
+        }
+    }
+
+    void ColorPicker()
+    {
+        if(type == 0)                  // normal room
+        {
+            mainColor = normalColor;
+        }
+        else if(type == 1)             // start room
+        {
+            mainColor = startColor;
+        }
+        else if (type == 2)            // active room color
+        {
+            mainColor = actualColor;
+        }
+        else if (type == 3)            // boss room
+        {
+            mainColor = bossColor;
+        }
+        else if (type == 4)            // treasure room
+        {
+            mainColor = treasureColor;
+        }
+        rend.color = mainColor;
+    }
+}
