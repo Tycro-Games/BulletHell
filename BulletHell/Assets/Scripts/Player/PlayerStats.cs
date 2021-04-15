d57c2f99f37efa24276f6fe24678a730acda99f4
NiceGuy <bogdan.game.development@gmail.com>
2021-04-15 19:12:54 +0300

    Minor fix

--- BulletHell/Assets/Scripts/Player/PlayerStats.cs
+++ BulletHell/Assets/Scripts/Player/PlayerStats.cs
@@ -3,19 +3,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using System;
+
 public class PlayerStats : CommonStats, IHitable
 {
     [SerializeField]
     private float UntilNextHit = .5f;
+
     [SerializeField]
     private bool immortal = false;
 
     //auxiliary
     public static bool atacked = false;
+
     private float currentTime = 0.0f;
     private float finishTime = 0.0f;
 
     public static event Action deathEvent;
+
     private void Update()
     {
         if (atacked)
@@ -25,19 +29,22 @@ public class PlayerStats : CommonStats, IHitable
                 atacked = false;
         }
     }
+
     public void Immortal()
     {
         immortal = !immortal;
     }
+
     private void OnEnable()
     {
         EnemyController.HitEvent += TakeDamage;
     }
+
     private void OnDisable()
     {
         EnemyController.HitEvent -= TakeDamage;
-
     }
+
     public void TakeDamage(int dg)
     {
         if (!immortal)
@@ -64,11 +71,8 @@ public class PlayerStats : CommonStats, IHitable
         }
     }
 
-
-
     public void Die()
     {
-
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);//restart
         deathEvent?.Invoke();
     }
