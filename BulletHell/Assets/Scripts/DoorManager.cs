1365896a037c3ee7da43e32b8bc5a8acc66e6cf9
NiceGuy <bogdan.game.development@gmail.com>
2021-04-18 09:41:09 +0300

    Last room with door bugs
    Make the fucking doors not buggy and by the way that room doesn't work always

---
+++ BulletHell/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    [HideInInspector]
+    public List<LastDoor> doors = new List<LastDoor>();
+
+    public void Activate()
+    {
+        for (int i = 0; i < doors.Count; i++)
+        {
+            doors[i].Activate();
+        }
+    }
+}
--- BulletHell/Assets/Scripts/LastDoor.cs
+++ BulletHell/Assets/Scripts/LastDoor.cs
@@ -4,4 +4,18 @@ using UnityEngine;
 
 public class LastDoor : MonoBehaviour
 {
+    private Collider2D doorCollide = null;
+
+    private void Start()
+    {
+        doorCollide = GetComponent<Collider2D>();
+        doorCollide.enabled = false;
+        DoorManager doorManager = FindObjectOfType<DoorManager>();
+        doorManager.doors.Add(this);
+    }
+
+    public void Activate()
+    {
+        doorCollide.enabled = true;
+    }
 }
