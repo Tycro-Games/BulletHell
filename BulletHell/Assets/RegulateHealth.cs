4963c246bfb33788e17832e4523b61c1255504eb
NiceGuy <bogdan.game.development@gmail.com>
2021-04-20 17:33:27 +0300

    Update luminata

---
+++ BulletHell/Assets/RegulateHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bog
+{
+    public class RegulateHealth : MonoBehaviour
+    {
+        [SerializeField]
+        private float Multiplier = 1f;
+
+        private Material material;
+
+        private void Start()
+        {
+            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+            sprite.sharedMaterial = new Material(sprite.sharedMaterial);
+        }
+
+        public void UpdateHealth(int HP)
+        {
+            material.shader.GetDependency("_color");
+        }
+    }
+}
