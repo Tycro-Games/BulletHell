c56a8df1f2a72ffc2df23ce2023644683accb762
AmiPopa <amipopa@gmail.com>
2021-04-22 17:48:57 +0300

    Moved stuff

--- BulletHell/Assets/Scripts/GameObjectSpawner.cs
+++ BulletHell/Assets/Scripts/GameObjectSpawner.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossSpawner : MonoBehaviour
+public class GameObjectSpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] Enemies;
