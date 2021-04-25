5524afa102ffa7fa1e18d0c5e19129ebb980c009
NiceGuy <bogdan.game.development@gmail.com>
2021-04-25 12:27:16 +0300

    Fucked up generation

---
+++ BulletHell/Assets/RandomPitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class RandomPitch : MonoBehaviour
+{
+    private AudioSource Audio = null;
+
+    [SerializeField]
+    private float minS = 1;
+
+    [SerializeField]
+    private float maxS = 1;
+
+    private void OnEnable()
+    {
+        Audio = GetComponent<AudioSource>();
+        ChangePitch();
+    }
+
+    public void ChangePitch()
+    {
+        Audio.pitch = Random.Range(minS, maxS);
+    }
+}
